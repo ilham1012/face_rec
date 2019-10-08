7e560f432f3e0b5cdedebf90c4c7efff5e3bfc7c
Ilham <myung1012@gmail.com>
2019-10-08 13:53:44 +0700

    Create custom Entry which manually trigger 'onboard' keyboard on focus

---
+++ doorlock/textbox.py
@@ -0,0 +1,19 @@
+import subprocess
+
+import tkinter.ttk as ttk
+
+class TextBox(ttk.Entry):
+    def __init__(self, master=None, widget=None, **kw):
+        super().__init__(master, widget, **kw)
+        self.bind('<FocusIn>', self.focus_in)
+        self.bind('<FocusOut>', self.focus_out)
+        
+
+
+    def focus_out(self, event):
+        print("[TextBox] Focus Out")
+        subprocess.Popen(["pkill", "onboard"])
+
+    def focus_in(self, event):
+        print("[TextBox] Focus In")
+        subprocess.Popen("onboard")
